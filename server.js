ab24d2751bd2409a1316e0520211ddef4f7cb23a
ChrissyDLC <chriscelle.delacruz@staffbase.com>
2025-12-18 03:25:13 -0500

    Add kicker field to server response contents

--- server.js
+++ server.js
@@ -220,7 +220,7 @@ app.post("/api/create", upload.single("taskCsv"), async (req, res) => {
         en_US: { 
           title: title, 
           content: contentHTML, 
-          topic: department 
+          kicker: department 
         } 
       }
     });
